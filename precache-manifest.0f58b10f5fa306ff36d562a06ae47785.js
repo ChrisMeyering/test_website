0147c8e5732a5ba2b294ea17a2712e6f84e7b62c
Chris Meyering <christophe.daven@gmail.com>
2019-10-12 10:52:26 -0700

    Updates

---
+++ precache-manifest.0f58b10f5fa306ff36d562a06ae47785.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a11d21b6cea5dc1a70edcb11cb7d8433",
+    "url": "/test_website/index.html"
+  },
+  {
+    "revision": "f34a7022d9365495f9e0",
+    "url": "/test_website/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "1145102afe459efc5fb7",
+    "url": "/test_website/static/js/2.15cbb02b.chunk.js"
+  },
+  {
+    "revision": "f34a7022d9365495f9e0",
+    "url": "/test_website/static/js/main.284c431d.chunk.js"
+  },
+  {
+    "revision": "d7d96baa95cbdcbdd7ff",
+    "url": "/test_website/static/js/runtime-main.c25955a8.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/test_website/static/media/logo.25bf045c.svg"
+  }
+]);
